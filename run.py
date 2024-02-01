9c24cc248280f9db7d2a8cafccdf907c315e3a75
yosephdev <>
2024-02-01 17:59:02 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -67,7 +67,7 @@ def calculate_surplus_data(sales_row):
     - Positive surplus indicates waste
     - Negative surplus indicates extra made when stock was sold out.
     """
-    print("Calculating surplus data...\n")
+    print("Calculating stock data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]   
 
@@ -94,6 +94,21 @@ def get_last_5_entires_sales():
     
     return columns
 
+def calculate_stock_data(data):
+    '''
+    Calculate the average stock for each item type, adding 10%
+    '''    
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
 def main():
     '''
     Run program functions
@@ -103,8 +118,9 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)    
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entires_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to Love Sandwitches Data Automation")
-# main()
-
-sales_columns = get_last_5_entires_sales()
+main()
